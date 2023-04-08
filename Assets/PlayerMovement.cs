b4e89456f76241788034f998ce2d2cebf5dd959d
Sacus <75908397+Sacus1@users.noreply.github.com>
2023-04-08 13:04:48 +0200

    Select cube

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,46 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public sealed class PlayerMovement : MonoBehaviour
 {
-	public   float               speed            = 5f;
-	public   float               jumpForce        = 5f;
-	public   float               mouseSensitivity = 10f;
-	private  Vector3             moveDirection;
-	internal Camera              cam;
-	internal float               camMax = 90;
-	internal float               camMin = -90;
-	private  CharacterController controller;
-	private const float          GRAVITY = 9.81f;
+	private const float               CAM_MAX          = 90;
+	private const float               CAM_MIN          = -90;
+	private const float               GRAVITY          = 9.81f;
+	public        float               speed            = 5f;
+	public        float               jumpForce        = 5f;
+	public        float               mouseSensitivity = 10f;
+	public        bool                isOffset;
+	public        float               selectDistance = 5f;
+	private       Camera              cam;
+	private       CharacterController controller;
+	private       Vector3             moveDirection;
+	public        bool                isRunning;
 	private void Start()
 	{
-		cam        = GetComponentInChildren<Camera>();
+		cam = GetComponentInChildren<Camera>();
 		// lock cursor
 		Cursor.lockState = CursorLockMode.Locked;
-		controller = GetComponent<CharacterController>();
+		controller       = GetComponent<CharacterController>();
 	}
-	public bool isOffset = false;
 	private void FixedUpdate()
 	{
 		controller.Move(transform.TransformDirection(moveDirection) * (speed * Time.deltaTime));
 		// apply gravity
 		moveDirection.y -= GRAVITY * Time.deltaTime;
-		if (!GetSquare(out RaycastHit _hit, out Vector3 _p0, out Vector3 _p1, out Vector3 _p2, out Vector3 _p3))
-			return;
-		// draw triangle
-		Debug.DrawLine(_p0, _p1, Color.red);
-		Debug.DrawLine(_p1, _p2, Color.red);
-		Debug.DrawLine(_p2, _p3, Color.red);
-		Debug.DrawLine(_p3, _p0, Color.red);
+		DrawCube();
 
 	}
-	private bool GetSquare(out RaycastHit _hit, out Vector3 _p0, out Vector3 _p1, out Vector3 _p2, out Vector3 _p3)
+	private void DrawCube()
 	{
-		_p0 = _p1 = _p2 = _p3 = Vector3.zero;
-		if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out _hit))
+		if (!GetCube(out RaycastHit _hit, out Vector3[] _points, selectDistance))
+			return;
+		// draw cube outline 
+		Color _color = Color.black;
+		Debug.DrawLine(_points[0], _points[1], _color);
+		Debug.DrawLine(_points[1], _points[2], _color);
+		Debug.DrawLine(_points[2], _points[3], _color);
+		Debug.DrawLine(_points[3], _points[0], _color);
+		Debug.DrawLine(_points[0], _points[4], _color);
+		Debug.DrawLine(_points[1], _points[5], _color);
+		Debug.DrawLine(_points[2], _points[6], _color);
+		Debug.DrawLine(_points[3], _points[7], _color);
+		Debug.DrawLine(_points[4], _points[5], _color);
+		Debug.DrawLine(_points[5], _points[6], _color);
+		Debug.DrawLine(_points[6], _points[7], _color);
+		Debug.DrawLine(_points[7], _points[4], _color);
+	}
+	private bool GetSquare(out RaycastHit _hit, out Vector3 _point0, out Vector3 _point1, out Vector3 _point2, out Vector3 _point3, float distance = Mathf.Infinity)
+	{
+		_point0 = _point1 = _point2 = _point3 = Vector3.zero;
+		if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out _hit, distance))
 			return false;
 
 		MeshCollider _meshCollider = _hit.collider as MeshCollider;
@@ -51,31 +63,112 @@ public sealed class PlayerMovement : MonoBehaviour
 		Vector3[] _vertices  = _mesh.vertices;
 		int[]     _triangles = _mesh.triangles;
 		// triangle are 0,2,1 and 0,3,2
-		_p0       = _vertices[_triangles[_hit.triangleIndex * 3 + 0]];
-		_p1       = _vertices[_triangles[_hit.triangleIndex * 3 + 2]];
-		_p2       = _vertices[_triangles[_hit.triangleIndex * 3 + 1]];
-		_p3       = _vertices[_triangles[_hit.triangleIndex * 3 + 4]];
+		_point0  = _vertices[_triangles[_hit.triangleIndex * 3 + 0]];
+		_point1  = _vertices[_triangles[_hit.triangleIndex * 3 + 2]];
+		_point2  = _vertices[_triangles[_hit.triangleIndex * 3 + 1]];
+		_point3  = _vertices[_triangles[_hit.triangleIndex * 3 + 4]];
 		isOffset = false;
 		// check that they form a square , if not offset
-		if (Vector3.Distance(_p0, _p1) > 1.1f || Vector3.Distance(_p1, _p2) > 1.1f || Vector3.Distance(_p2, _p3) > 1.1f || Vector3.Distance(_p3, _p0) > 1.1f)
+		if (Vector3.Distance(_point0, _point1) > 1.1f || Vector3.Distance(_point1, _point2) > 1.1f || Vector3.Distance(_point2, _point3) > 1.1f || Vector3.Distance(_point3, _point0) > 1.1f)
 		{
 			isOffset = true;
-			int _offset = 3;
-			_p0 = _vertices[_triangles[_hit.triangleIndex * 3 + 0 + _offset]];
-			_p1 = _vertices[_triangles[_hit.triangleIndex * 3 + 2 + _offset]];
-			_p2 = _vertices[_triangles[_hit.triangleIndex * 3 + 1 + _offset]];
-			_p3 = _vertices[_triangles[_hit.triangleIndex * 3 + 4 + _offset]];
-			return false;
+			const int _offset = -3;
+			_point0 = _vertices[_triangles[_hit.triangleIndex * 3 + 0 + _offset]];
+			_point1 = _vertices[_triangles[_hit.triangleIndex * 3 + 2 + _offset]];
+			_point2 = _vertices[_triangles[_hit.triangleIndex * 3 + 1 + _offset]];
+			_point3 = _vertices[_triangles[_hit.triangleIndex * 3 + 4 + _offset]];
 		}
 
 		// convert to world space
-		_p0 = _meshCollider.transform.TransformPoint(_p0);
-		_p1 = _meshCollider.transform.TransformPoint(_p1);
-		_p2 = _meshCollider.transform.TransformPoint(_p2);
-		_p3 = _meshCollider.transform.TransformPoint(_p3);
+		_point0 = _meshCollider.transform.TransformPoint(_point0);
+		_point1 = _meshCollider.transform.TransformPoint(_point1);
+		_point2 = _meshCollider.transform.TransformPoint(_point2);
+		_point3 = _meshCollider.transform.TransformPoint(_point3);
 		return true;
 	}
+	private bool GetCube(out RaycastHit _hit, out Vector3[] _points, float distance)
+	{
+		if (!GetSquare(out _hit, out Vector3 _p0, out Vector3 _p1, out Vector3 _p2, out Vector3 _p3, distance))
+		{
+			_points = null;
+			return false;
+		}
+		_points = new Vector3[8];
 
+		// get the normal of the square
+		Vector3 _normal = Vector3.Cross(_p1 - _p0, _p3 - _p0).normalized;
+
+		// if face is top
+		if (_normal == Vector3.down)
+		{
+			_points[0] = _p0;
+			_points[1] = _p1;
+			_points[2] = _p2;
+			_points[3] = _p3;
+			_points[4] = _p0 - Vector3.up;
+			_points[5] = _p1 - Vector3.up;
+			_points[6] = _p2 - Vector3.up;
+			_points[7] = _p3 - Vector3.up;
+		}
+
+		// if face is left
+		if (_normal == Vector3.right)
+		{
+			_points[0] = _p0;
+			_points[1] = _p1;
+			_points[2] = _p2;
+			_points[3] = _p3;
+			_points[4] = _p0 + Vector3.right;
+			_points[5] = _p1 + Vector3.right;
+			_points[6] = _p2 + Vector3.right;
+			_points[7] = _p3 + Vector3.right;
+		}
+
+		// if face is right
+		if (_normal == Vector3.left)
+		{
+			_points[0] = _p0;
+			_points[1] = _p1;
+			_points[2] = _p2;
+			_points[3] = _p3;
+			_points[4] = _p0 + Vector3.left;
+			_points[5] = _p1 + Vector3.left;
+			_points[6] = _p2 + Vector3.left;
+			_points[7] = _p3 + Vector3.left;
+		}
+
+		// if face is front
+		if (_normal == Vector3.back)
+		{
+			_points[0] = _p0;
+			_points[1] = _p1;
+			_points[2] = _p2;
+			_points[3] = _p3;
+			_points[4] = _p0 + Vector3.back;
+			_points[5] = _p1 + Vector3.back;
+			_points[6] = _p2 + Vector3.back;
+			_points[7] = _p3 + Vector3.back;
+		}
+
+		// if face is back
+		if (_normal == Vector3.forward)
+		{
+			_points[0] = _p0;
+			_points[1] = _p1;
+			_points[2] = _p2;
+			_points[3] = _p3;
+			_points[4] = _p0 + Vector3.forward;
+			_points[5] = _p1 + Vector3.forward;
+			_points[6] = _p2 + Vector3.forward;
+			_points[7] = _p3 + Vector3.forward;
+		}
+
+		// if face is bottom TODO: je peut pas test ca car je ne peux pas placer un cube sur le sol
+
+		return true;
+
+
+	}
 	private void OnMove(InputValue _value)
 	{
 		Vector2 _input = _value.Get<Vector2>();
@@ -86,9 +179,7 @@ public sealed class PlayerMovement : MonoBehaviour
 	private void OnJump()
 	{
 		if (controller.isGrounded)
-		{
 			moveDirection.y = jumpForce;
-		}
 	}
 
 	private void OnLook(InputValue _value)
@@ -98,10 +189,15 @@ public sealed class PlayerMovement : MonoBehaviour
 
 		Vector2 _input = _value.Get<Vector2>();
 		transform.Rotate(Vector3.up, _input.x);
-		float _angle  = cam.transform.localEulerAngles.x - _input.y * Time.deltaTime * mouseSensitivity;
+		float _angle = cam.transform.localEulerAngles.x - _input.y * Time.deltaTime * mouseSensitivity;
 		// convert from 0-360 to -180-180
-		_angle                          = _angle > 180 ? _angle - 360 : _angle;
-		_angle                          = Mathf.Max(Mathf.Min(_angle, camMax), camMin);
+		_angle                         = _angle > 180 ? _angle - 360 : _angle;
+		_angle                         = Mathf.Max(Mathf.Min(_angle, CAM_MAX), CAM_MIN);
 		cam.transform.localEulerAngles = new(_angle, 0, 0);
 	}
+	private void OnRun()
+	{
+		isRunning = !isRunning;
+	}
+
 }
