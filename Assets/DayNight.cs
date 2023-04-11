b977d80f38f5e0aecf890700dae1c3500c8ffcc8
Sacus <75908397+Sacus1@users.noreply.github.com>
2023-04-11 20:26:47 +0200

    better sky

--- Assets/DayNight.cs
+++ Assets/DayNight.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 public sealed class DayNight : MonoBehaviour
 {
+	private static readonly int BLEND = Shader.PropertyToID("_Blend");
 	[Range(0, 24)]
 	public float timeOfDay = 12;
 	public  float     dayLength = 180f; // seconds
 	public  float     angle;
 	public  Transform reference;
+	public  Material  daySkybox;
+	public  Material  nightSkybox;
+	public  int       sunRise = 6;
+	public  int       sunSet  = 18;
 	private Light     sun;
 	private void Start()
 	{
-		sun       = GetComponent<Light>();
-		timeOfDay = 12;
+		sun                   = GetComponent<Light>();
+		timeOfDay             = 12;
+		RenderSettings.skybox = daySkybox;
+		RenderSettings.sun    = sun;
 	}
 
 	private void FixedUpdate()
@@ -20,12 +27,6 @@ public sealed class DayNight : MonoBehaviour
 		// set the intensity of the sun based on the time of day
 		float _intensity = 1 - Mathf.Abs(timeOfDay / 12 - 1) + 0.2f;
 		sun.intensity = _intensity;
-		// set the ambient light based on the time of day
-		RenderSettings.ambientLight = new(_intensity, _intensity, _intensity);
-		// set the fog color based on the time of day
-		RenderSettings.fogColor = new(_intensity, _intensity, _intensity);
-		// set the fog density based on the time of day
-		RenderSettings.fogDensity = 1 - _intensity;
 		// set the sun color based on the time of day
 		sun.color = new(_intensity, _intensity, _intensity);
 		// set the sun position 
@@ -38,5 +39,14 @@ public sealed class DayNight : MonoBehaviour
 		sun.transform.position = new Vector3(_x, _y, _z) + reference.position;
 		// set the sun rotation
 		sun.transform.LookAt(Vector3.zero);
+		// set the skybox based on the time of day
+		if (timeOfDay >= sunRise && timeOfDay <= sunSet)
+			RenderSettings.skybox = daySkybox;
+		else
+			RenderSettings.skybox = nightSkybox;
+
+		// set blend factor for the skybox
+		RenderSettings.skybox.SetFloat(BLEND, Mathf.Abs(timeOfDay / 12 - 1));
+
 	}
 }
